086aacf96cd37c2db7615ab5c12a1e9052866fa5
Anshu-007 <131807617+Anshu-007@users.noreply.github.com>
2024-10-09 22:35:40 +0530

    implement the complete layout

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -1,44 +1,87 @@
-'use client'
+"use client";
 import React from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { sidebarLinks } from "@/constants";
 import { cn } from "@/lib/utils";
 import {
-    Sheet,
-    SheetContent,
-    SheetDescription,
-    SheetHeader,
-    SheetTitle,
-    SheetTrigger,
-  } from "@/components/ui/sheet"
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+  SheetClose,
+} from "@/components/ui/sheet";
 import { usePathname } from "next/navigation";
-  
+
 const MobileNav = ({ user }: MobileNavProps) => {
-    const pathname = usePathname()
+  const pathname = usePathname();
   return (
     <section className="w-full max-w-[264px]">
       <Sheet>
         <SheetTrigger>
-            <Image src="/icons/hamburger.svg" height={30} width={30} alt="menu" className="cursor-pointer"/>
+          <Image
+            src="/icons/hamburger.svg"
+            height={30}
+            width={30}
+            alt="menu"
+            className="cursor-pointer"
+          />
         </SheetTrigger>
-        <SheetContent side="left">
-            <Link href='/' className="mb-12 cursor-pointer flex items-center gap-2">
-                <Image src="/icons/logo.svg" height={34} width={34} alt="logo" className="size-[24px] max-xl-size-14"/>
-                <h1 className="sidebar-logo">Horizon</h1>
-            </Link>
-            {sidebarLinks.map((items)=>{
-                const isActive = pathname === items.route || pathname.startsWith(`${items.route}/`)
-                return <Link href={items.route} key={items.label} className={cn("sidebar-link",{'bg-bank-gradient' : isActive})}>
-                    <div className='relative size-6'> <Image src={items.imgURL} alt={items.label} fill className={cn({'brightness-[3] invert-0':isActive} )}/></div>
-                    <p className={cn('sidebar-label',{'!text-white' : isActive})}>
+        <SheetContent side="left" className="border-none bg-white">
+          <Link
+            href="/"
+            className=" cursor-pointer flex items-center gap-1 px-4"
+          >
+            <Image src="/icons/logo.svg" height={34} width={34} alt="logo" />
+            <h1 className="text-26 font-ibm-plex-serif font-bold text-black-1">
+              Horizon
+            </h1>
+          </Link>
+          <div className="mobilenav-sheet">
+            <SheetClose asChild>
+              <nav className="flex h-full flex-col pt-16 gap-6 text-white">
+                {sidebarLinks.map((items) => {
+                  const isActive =
+                    pathname === items.route ||
+                    pathname.startsWith(`${items.route}/`);
+                  return (
+                    <SheetClose asChild key={items.route}>
+                      <Link
+                        href={items.route}
+                        key={items.label}
+                        className={cn("mobilenav-sheet_close w-full", {
+                          "bg-bank-gradient": isActive,
+                        })}
+                      >
+                        <Image
+                          src={items.imgURL}
+                          alt={items.label}
+                          
+                          height={20}
+                          width={20}
+                          className={cn({
+                            "brightness-[3] invert-0": isActive,
+                          })}
+                        />
 
-                    {items.label}
-                    </p>
-                    </Link>
-            })}
-            
-        
+                        <p
+                          className={cn("text-16 font-semibold text-black-2", {
+                            "text-white": isActive,
+                          })}
+                        >
+                          {items.label}
+                        </p>
+                      </Link>
+                    </SheetClose>
+                  );
+                })}
+                User
+              </nav>
+            </SheetClose>
+            Footer
+          </div>
         </SheetContent>
       </Sheet>
     </section>
